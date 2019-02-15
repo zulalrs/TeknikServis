a559c5ecae2638d3c6d91caa73acf3ad19778356
gozde <gozde.anagul@outlook.com>
2019-02-15 09:56:08 +0300

    Telefon ve Adres Kullanıcıya eklendi

--- TeknikServis.Models/ViewModels/RegisterViewModel.cs
+++ TeknikServis.Models/ViewModels/RegisterViewModel.cs
@@ -13,25 +13,39 @@ namespace TeknikServis.Models.ViewModels
         [Display(Name = "Ad")]
         [StringLength(25)]
         public string Name { get; set; }
+
         [StringLength(35)]
         [Required]
         [Display(Name = "Soyad")]
         public string Surname { get; set; }
+
         [Required]
         [Display(Name = "Kullanıcı Adı")]
         public string UserName { get; set; }
+
         [Required]
         [EmailAddress]
         public string Email { get; set; }
+
+        [StringLength(100)]
+        public string Adress { get; set; }
+
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Telefon numarasi sadece sayilardan olusmalidir")]
+        [StringLength(10, ErrorMessage = "Telefon numarasi 10 haneden fazla olamaz")]
+        public string Telefon { get; set; }
+
+
         [Required]
         [StringLength(100, MinimumLength = 5, ErrorMessage = "Şifreniz en az 5 karakter olmalıdır!")]
         [Display(Name = "Şifre")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
+
         [Required]
         [DataType(DataType.Password)]
         [Display(Name = "Şifre Tekrar")]
         [Compare("Password", ErrorMessage = "Şifreler uyuşmuyor")]
+
         public string ConfirmPassword { get; set; }
     }
 }
