462688b369ff7d5f72a2f83d5e023e34a7e5c4e9
zulalrs <zulal.r.s@gmail.com>
2019-02-15 09:02:11 +0300

    Register

---
+++ TeknikServisWeb/Controllers/AccountController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using TeknikServis.Models.IdentityModels;
+using TeknikServis.Models.ViewModels;
+using static TeknikServis.BLL.Identity.MembershipTools;
+
+namespace TeknikServisWeb.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            if (HttpContext.GetOwinContext().Authentication.User.Identity.IsAuthenticated)
+                return RedirectToAction("Index", "Home");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Register(RegisterLoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Index", model);
+            }
+            try
+            {
+                var userStore = NewUserStore();
+                var userManager = NewUserManager();
+
+                var rm = model.RegisterViewModel;
+
+                var user = await userManager.FindByNameAsync(rm.UserName);
+                if (user != null)
+                {
+                    ModelState.AddModelError("UserName", "Bu kullanıcı adı daha önceden alınmıştır");
+                    return View("Index", model);
+                }
+
+                var newUser = new User()
+                {
+                    UserName = rm.UserName,
+                    Email = rm.Email,
+                    Name = rm.Name,
+                    Surname = rm.Surname,
+                    ActivationCode = StringHelpers.GetCode()
+                };
+                var result = await userManager.CreateAsync(newUser, rm.Password);
+                if (result.Succeeded)
+                {
+                    if (userStore.Users.Count() == 1)
+                    {
+                        await userManager.AddToRoleAsync(newUser.Id, "Admin");
+                    }
+                    else
+                    {
+                        await userManager.AddToRoleAsync(newUser.Id, "User");
+                    }
+
+                    string SiteUrl = Request.Url.Scheme + System.Uri.SchemeDelimiter + Request.Url.Host +
+                                     (Request.Url.IsDefaultPort ? "" : ":" + Request.Url.Port);
+
+                    var emailService = new EmailService();
+                    var body = $"Merhaba <b>{newUser.Name} {newUser.Surname}</b><br>Hesabınızı aktif etmek için aşadıdaki linke tıklayınız<br> <a href='{SiteUrl}/account/activation?code={newUser.ActivationCode}' >Aktivasyon Linki </a> ";
+                    await emailService.SendAsync(new IdentityMessage() { Body = body, Subject = "Sitemize Hoşgeldiniz" }, newUser.Email);
+                }
+                else
+                {
+                    var err = "";
+                    foreach (var resultError in result.Errors)
+                    {
+                        err += resultError + " ";
+                    }
+                    ModelState.AddModelError("", err);
+                    return View("Index", model);
+                }
+
+                TempData["Message"] = "Kaydınız alınlıştır. Lütfen giriş yapınız";
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                TempData["Model"] = new ErrorViewModel()
+                {
+                    Text = $"Bir hata oluştu {ex.Message}",
+                    ActionName = "Index",
+                    ControllerName = "Account",
+                    ErrorCode = 500
+                };
+                return RedirectToAction("Error", "Home");
+            }
+        }
+    }
+}
