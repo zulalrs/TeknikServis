1fccbd7b9d97e060450a69838fcc8033a6be27dd
gözde anagül <gozde.anagul@outlook.com>
2019-02-21 14:16:32 +0300

    Teknisyen Detay

--- TeknikServisWeb/Controllers/TeknisyenController.cs
+++ TeknikServisWeb/Controllers/TeknisyenController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TeknikServis.BLL.Repository;
+using TeknikServis.Models.ViewModels;
 
 namespace TeknikServisWeb.Controllers
 {
@@ -12,6 +14,58 @@ namespace TeknikServisWeb.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        [HttpGet]
+        public ActionResult GetAriza()
+        {
+            var data = new List<UserMarkaModelViewModel>();
+            var ariza = new ArizaRepository().GetAll(x => x.ArizaOnaylandiMi == false)
+                .ToList();
+            foreach (var x in ariza)
+            {
+                data.Add(new UserMarkaModelViewModel()
+                {
+                    Id = x.Id,
+                    MusteriAdi = x.Musteri.Name + " " + x.Musteri.Surname,
+                    Adres = x.Adres,
+                    Aciklama = x.Aciklama,
+                    //EklemeTarihi = x.EklemeTarihi
+                });
+            }
+            return View(data);
+        }
+
+
+
+        //[HttpPost]
+        //public ActionResult GetAriza(UserMarkaModelViewModel model)
+        //{
+        //    return View(model);
+        //}
+
+
+        [HttpGet]
+        public ActionResult GetArizaDetay()
+        {
+            var data = new List<UserMarkaModelViewModel>();
+            var ariza = new ArizaRepository().GetAll(x => x.ArizaOnaylandiMi == true)
+                .ToList();
+            foreach (var x in ariza)
+            {
+                data.Add(new UserMarkaModelViewModel()
+                {
+                    Id = x.Id,
+                    MusteriAdi = x.Musteri.Name + " " + x.Musteri.Surname,
+                    Adres = x.Adres,
+                    Aciklama = x.Aciklama,
+                    ArizaFoto = x.ArizaFoto,
+                    GarantiliVarMi = x.GarantiliVarMi,
+                    MarkaAdi = x.Model.Marka.MarkaAdi,
+                    ModelAdi = x.Model.ModelAdi
+                });
+            }
+            return View(data);
+
         }
     }
 }
